48f4966509a74ad74d14ae4da023a0a2b8e8b6b4
Byungsoo Kim <kimby@inf.ethz.ch>
2018-05-08 10:40:48 +0200

    change default setting of smoke_pos_size scene

--- scene.bat
+++ scene.bat
@@ -1,4 +1,4 @@
-..\manta\build_nogui_vdb\Release\manta.exe ./scene/smoke3_vel_buo.py 
+REM ..\manta\build_nogui_vdb\Release\manta.exe ./scene/smoke3_vel_buo.py 
 
 REM ..\manta\build_nogui\Release\manta.exe ./scene/smoke_mov.py
 
@@ -7,7 +7,7 @@ REM ..\manta\build\Release\manta.exe ./scene/smoke_jet.py --data_gen=False
 REM ..\manta\build_nogui_vdb\Release\manta.exe ./scene/smoke3_pos.py --num_src_x_pos=10 --num_frames=100 --max_frames=99 --num_simulations=1000 --clamp_mode=1 --time_step=1 --log_dir=D:/Polybox/dev/deep-fluids/data/smoke3_pos10_f100
 
 REM ..\manta\build_nogui\Release\manta.exe ./scene/liquid_pos_size.py --num_src_x_pos=10 --num_src_radius=4 --num_frames=200 --max_frames=199 --num_simulations=8000 --log_dir=data/liquid_pos10_size4_f200
-REM ..\manta\build_nogui\Release\manta.exe ./scene/smoke_pos_size.py --num_src_x_pos=21 --num_src_radius=5 --num_frames=200 --max_frames=199 --num_simulations=21000 --log_dir=data/smoke_pos21_size5_f200
+..\manta\build_nogui\Release\manta.exe ./scene/smoke_pos_size.py
 REM ..\manta\build_nogui\Release\manta.exe ./scene/smoke_pos.py --num_src_x_pos=10 --num_frames=200 --max_frames=199 --num_simulations=2000 --clamp_mode=2 --time_step=0.5 --strength=0.1 --log_dir=D:/Polybox/dev/deep-fluids/data/smoke_pos10_f100_vc
 REM ..\manta\build_nogui\Release\manta.exe ./scene/smoke_pos.py --num_src_x_pos=10 --num_frames=100 --max_frames=99 --num_simulations=1000 --clamp_mode=1 --time_step=1 --log_dir=D:/Polybox/dev/deep-fluids/data/smoke_pos10_f100
 
